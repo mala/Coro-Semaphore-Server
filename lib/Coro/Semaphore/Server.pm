45127ba96c08d19cf642caa1e054cd7b68aa8940
mala <github@ma.la>
2009-07-17 22:01:06 +0900

    initial commit

---
+++ lib/Coro/Semaphore/Server.pm
@@ -0,0 +1,33 @@
+package Coro::Semaphore::Server;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Coro::Semaphore::Server -
+
+=head1 SYNOPSIS
+
+  use Coro::Semaphore::Server;
+
+=head1 DESCRIPTION
+
+Coro::Semaphore::Server is
+
+=head1 AUTHOR
+
+mala E<lt>cpan@ma.laE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
